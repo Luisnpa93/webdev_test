89afbdacda4e1296b20b745f5bf37874a06fced8
Luisnpa93 <luis93.npa@gmail.com>
2023-03-21 14:42:16 +0000

    navbar style

--- client/src/components/pages/NavBar.tsx
+++ client/src/components/pages/NavBar.tsx
@@ -13,29 +13,32 @@ const NavBar = () => {
     window.location.reload(); // reload the page after navigating to /login
   };
   
-
   return (
     <>
       {user && (
-        <nav className="bg-light">
+        <nav className="bg-gradient-to-r from-blue-500 to-purple-500">
           <div className="container mx-auto">
             <div className="flex justify-between items-center py-4">
-              <Link to="/" className="text-lg font-bold">
+              <Link to="/" className="text-2xl font-bold text-white">
                 The best WebSite Ever!
               </Link>
               <button
-                className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600"
+                className="text-gray-200 hover:text-white focus:outline-none focus:text-white"
                 onClick={handleLogout}
               >
                 Logout
               </button>
             </div>
             <ul className="flex justify-end space-x-8">
-              <li className="text-gray-500 hover:text-gray-600">
-                <Link to="/option-a">Option A - empty</Link>
+              <li>
+                <Link to="/option-a" className="text-gray-200 hover:text-white text-lg font-medium">
+                  Option A
+                </Link>
               </li>
-              <li className="text-gray-500 hover:text-gray-600">
-                <Link to="/option-b">Option B - empty</Link>
+              <li>
+                <Link to="/option-b" className="text-gray-200 hover:text-white text-lg font-medium">
+                  Option B
+                </Link>
               </li>
             </ul>
           </div>
